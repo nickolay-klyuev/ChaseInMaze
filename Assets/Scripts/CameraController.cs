92cb7d6276de8ab813d97b0df5a4d20c7b2a9bc6
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-04-12 15:12:16 +0300

    add camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speedOfMoving = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Up"))
+        {
+            transform.position += new Vector3(0, 0, speedOfMoving) * Time.deltaTime;
+        }
+        else if (Input.GetButton("Down"))
+        {
+            transform.position += new Vector3(0, 0, -speedOfMoving) * Time.deltaTime;
+        }
+        else if (Input.GetButton("Left"))
+        {
+            transform.position += new Vector3(-speedOfMoving, 0, 0) * Time.deltaTime;
+        }
+        else if (Input.GetButton("Right"))
+        {
+            transform.position += new Vector3(speedOfMoving, 0, 0) * Time.deltaTime;
+        }
+    }
+}
