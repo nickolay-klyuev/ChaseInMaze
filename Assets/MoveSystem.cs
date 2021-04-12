bfa4289b9f82e2e4941a52efd0abb0ff1b45c7b8
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-04-12 13:38:17 +0300

    add moving

--- Assets/MoveSystem.cs
+++ Assets/MoveSystem.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MoveSystem : MonoBehaviour
 {
+    public float moveSpeed = 1f;
+
+    public float xTarget = 10;
+    public float zTarget = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +16,9 @@ public class MoveSystem : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        float step = moveSpeed * Time.deltaTime;
+        Vector3 target = new Vector3(xTarget, transform.position.y, zTarget);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
     }
 }
