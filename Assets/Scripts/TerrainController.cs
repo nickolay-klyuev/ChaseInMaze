c47500aa8239974429e13dc18f1ad9cad54e09a4
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-04-13 18:38:42 +0300

    change move object method

--- Assets/Scripts/TerrainController.cs
+++ Assets/Scripts/TerrainController.cs
@@ -31,7 +31,25 @@ public class TerrainController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
-            float yCamera = Camera.main.transform.position.y;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            foreach(ObjectController objectController in objectControllers)
+            {
+                if (objectController.GetIsSelected())
+                {
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        objectController.xTarget = hit.point.x;
+                        objectController.zTarget = hit.point.z;
+                    }
+                }
+            }
+
+
+
+            // One more method just in case
+            /*float yCamera = Camera.main.transform.position.y;
             Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, yCamera));
 
             foreach(ObjectController objectController in objectControllers)
@@ -41,7 +59,7 @@ public class TerrainController : MonoBehaviour
                     objectController.xTarget = target.x;
                     objectController.zTarget = target.z;
                 }
-            }
+            }*/
         }
     }
 }
