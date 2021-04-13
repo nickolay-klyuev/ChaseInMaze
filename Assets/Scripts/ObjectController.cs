7017982ea162dc3c069a1cba4542db556c90e949
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-04-13 13:12:16 +0300

    creating select object system

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public Material selectedMaterial;
+    public Material defaultMaterial;
+
+    private bool isSelected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().material = defaultMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSelected)
+        {
+            ChangeMaterial("selected");
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isSelected = true;
+    }
+
+    public void setIsSelected(bool selected)
+    {  
+        isSelected = selected;
+    }
+
+    public void ChangeMaterial(string materialType = "default")
+    {
+        if (materialType == "selected")
+        {
+            GetComponent<MeshRenderer>().material = selectedMaterial;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material = defaultMaterial;
+        }
+    }
+}
